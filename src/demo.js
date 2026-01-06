0bc3b0e2c0e037ba59fbb1ec136618183e7e858f
kirankum1310 <kirankum1310@gmail.com>
2026-01-06 12:44:34 +0530

    Remove yn package and related files from the project
    - Deleted the yn package files including license, package.json, and readme.md.
    - Removed references to yn in package-lock.json and package.json.
    - Cleaned up TypeScript configuration files and added a new tsconfig.json with updated settings.
    - Added a .gitignore file to exclude common node and TypeScript build artifacts.

--- src/demo.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let user = "Playwright Learner";
-console.log(`Welcome, ${user}! Welcome to the Playwright demo.`);
-//# sourceMappingURL=demo.js.map
--- src/demo.ts
+++
@@ -1,2 +0,0 @@
-let user:string = "Playwright Learner";
-console.log(`Welcome, ${user}! Welcome to the Playwright demo.`);
